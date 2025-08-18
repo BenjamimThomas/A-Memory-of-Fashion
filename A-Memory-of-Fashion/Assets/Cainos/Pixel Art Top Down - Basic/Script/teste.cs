d93d64e9c7f0d1fee9cff692997e5d9fc1a7327d
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-08-18 14:38:55 -0300

    jornais

--- A-Memory-of-Fashion/Assets/Cainos/Pixel Art Top Down - Basic/Script/teste.cs
+++ A-Memory-of-Fashion/Assets/Cainos/Pixel Art Top Down - Basic/Script/teste.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class teste : MonoBehaviour
 {
+    public Button btn;
     public void Cliquei()
     {
         Debug.Log("O botão foi clicado!");
