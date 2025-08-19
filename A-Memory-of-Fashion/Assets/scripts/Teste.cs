62f0d563ee207ba0cdecbc5199327d7b56bf3e4a
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-08-19 15:51:39 -0300

    New Menu

--- A-Memory-of-Fashion/Assets/scripts/Teste.cs
+++ A-Memory-of-Fashion/Assets/scripts/Teste.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
-using TMPro;
+//using TMPro;
 using UnityEngine.UI;
 
-public class teste : MonoBehaviour
+public class Teste : MonoBehaviour
 {
-    public Button btn;
+    //public Button btn;
     public void Cliquei()
     {
         Debug.Log("O botão foi clicado!");
