adc28859b324a0b9921d06bd30f4b678cc793614
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-11-04 13:50:16 -0300

    Menu corrigido
    falta a correção dos colliders

--- A-Memory-of-Fashion/Assets/Benjamim/Scripts/CreditsTrigger.cs
+++ A-Memory-of-Fashion/Assets/Benjamim/Scripts/CreditsTrigger.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string creditsSceneName = "Creditos";
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(creditsSceneName);
+        }
     }
 }
--- A-Memory-of-Fashion/Assets/Benjamim/Scripts/StartSceneTrigger.cs
+++ A-Memory-of-Fashion/Assets/Benjamim/Scripts/StartSceneTrigger.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartSceneTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string casaSceneName = "Casa 1";
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(casaSceneName);
+        }
     }
 }
+
