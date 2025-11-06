e1ae6335a6093bb0a6805f1ded891ecd6e6db91d
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-11-06 16:09:44 -0300

    script inventory exemplificado

--- A-Memory-of-Fashion/Assets/Benjamim/Scripts/InventoryUI.cs
+++ A-Memory-of-Fashion/Assets/Benjamim/Scripts/InventoryUI.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
-using System.Collections; 
+
 public class InventoryUI : MonoBehaviour
 {
     public GameObject inventoryPanel;
     private bool isOpen = false;
-    private Animator animator;
 
     void Start()
     {
-        animator = inventoryPanel.GetComponent<Animator>();
         inventoryPanel.SetActive(false);
     }
 
@@ -16,34 +14,8 @@ public class InventoryUI : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if (isOpen)
-                CloseInventory();
-            else
-                OpenInventory();
-        }
-    }
-
-    void OpenInventory()
-    {
-        inventoryPanel.SetActive(true);
-        animator.SetBool("IsOpen", true); 
-        isOpen = true;
-    }
-    void CloseInventory()
-    {
-        animator.SetBool("IsOpen", false);
-
-        StopAllCoroutines();
-        StartCoroutine(HideAfterDelay());
-
-        isOpen = false;
-    }
-    IEnumerator HideAfterDelay()
-    {
-        yield return new WaitForSeconds(0.5f);
-        if (!isOpen)
-        {
-            inventoryPanel.SetActive(false);
+            isOpen = !isOpen;
+            inventoryPanel.SetActive(isOpen);
         }
     }
 }
