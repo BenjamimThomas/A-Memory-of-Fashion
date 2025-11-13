09c8a46b8b0c91d001ad1f9e84e9cc8a81c42825
Jancley222 <jancley.hermes@alunos.sc.senac.br>
2025-11-13 13:41:01 -0300

    TENTANDO ANIMAR

---
+++ A-Memory-of-Fashion/Assets/Jancley/PlayerAnimationController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+
+    public Animator playerAnimator;
+
+    public void PlayAnimation(string animationName)
+    {
+        playerAnimator.Play(animationName);
+    }
+}
