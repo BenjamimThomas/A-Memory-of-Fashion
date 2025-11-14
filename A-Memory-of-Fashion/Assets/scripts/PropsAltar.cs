e18a09a89dc0661c0b6baa4fab308c3f6aa14be9
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-11-14 10:45:15 -0300

    Reapply "merge"
    This reverts commit c0c0271123d8285125efa554d00e04d4588467df.

--- A-Memory-of-Fashion/Assets/scripts/PropsAltar.cs
+++ A-Memory-of-Fashion/Assets/scripts/PropsAltar.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 namespace Cainos.PixelArtTopDown_Basic
 {
-
     public class PropsAltar : MonoBehaviour
     {
         public List<SpriteRenderer> runes;
         public float lerpSpeed;
+        public GameObject teleportButton;
+        public string sceneToLoad;
 
         private Color curColor;
         private Color targetColor;
@@ -17,15 +18,18 @@ namespace Cainos.PixelArtTopDown_Basic
         private void Awake()
         {
             targetColor = runes[0].color;
+            teleportButton.SetActive(false);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
+            if (!other.CompareTag("Caixa")) return;
             targetColor.a = 1.0f;
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
+            if (!other.CompareTag("Caixa")) return;
             targetColor.a = 0.0f;
         }
 
@@ -37,6 +41,16 @@ namespace Cainos.PixelArtTopDown_Basic
             {
                 r.color = curColor;
             }
+
+            if (curColor.a > 0.95f)
+                teleportButton.SetActive(true);
+            else
+                teleportButton.SetActive(false);
+        }
+
+        public void Teleport()
+        {
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
