c8f90255390576f0f200275ea01374ce2da6215f
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-11-15 17:52:50 -0300

    inventory UI mudado

--- A-Memory-of-Fashion/Assets/Benjamim/Scripts/InventoryUI.cs
+++ A-Memory-of-Fashion/Assets/Benjamim/Scripts/InventoryUI.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
+    [Header("Painel do Inventário")]
     public GameObject inventoryPanel;
+
+    [Header("Slots dos Itens na UI")]
+    public GameObject slotVestidoVermelho;
+    public GameObject slotVestidoAzul;
+    public GameObject slotSapatoVermelho;
+    public GameObject slotSapatoAzul;
+
     private bool isOpen = false;
 
     void Start()
     {
         inventoryPanel.SetActive(false);
+        AtualizarUI();
     }
 
     void Update()
@@ -16,6 +25,22 @@ public class InventoryUI : MonoBehaviour
         {
             isOpen = !isOpen;
             inventoryPanel.SetActive(isOpen);
+
+            if (isOpen)
+                AtualizarUI();
         }
+    }
+
+    public void AtualizarUI()
+    {
+        if (Inventory.instance == null)
+            return;
+
+        var inv = Inventory.instance;
+
+        slotVestidoVermelho.SetActive(inv.temVestidoVermelho);
+        slotVestidoAzul.SetActive(inv.temVestidoAzul);
+        slotSapatoVermelho.SetActive(inv.temSapatoVermelho);
+        slotSapatoAzul.SetActive(inv.temSapatoAzul);
     }
 }
