5a84ae3069c1c91e5dc9159fe5c46891d5793cf4
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-11-15 19:46:48 -0300

    mudanças sprites player
    scenes top down dentro da casa

--- A-Memory-of-Fashion/Assets/Benjamim/Scripts/ItemPickup.cs
+++ A-Memory-of-Fashion/Assets/Benjamim/Scripts/ItemPickup.cs
@@ -2,43 +2,37 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-    public string itemName; 
+    public string itemName;
     private bool playerNear = false;
+
     void Start()
-{
-    if (PlayerPrefs.GetInt(itemName, 0) == 1)
     {
-        Destroy(gameObject);
-    }
-}
-    private void Update()
-    {
-        if (playerNear && Input.GetKeyDown(KeyCode.E))
+        if (PlayerPrefs.GetInt(itemName, 0) == 1)
         {
-            Inventory.instance.AddItem(itemName);
             Destroy(gameObject);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void Update()
+    {
+        if (playerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            Inventory.instance.AddItem(itemName);
+            PlayerPrefs.SetInt(itemName, 1);
+            PlayerPrefs.Save();
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
             playerNear = true;
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
             playerNear = false;
     }
-    if (playerNear && Input.GetKeyDown(KeyCode.E))
-{
-    Inventory.instance.AddItem(itemName);
-
-    PlayerPrefs.SetInt(itemName, 1);
-    PlayerPrefs.Save();
-
-    Destroy(gameObject);
-}
-
 }
