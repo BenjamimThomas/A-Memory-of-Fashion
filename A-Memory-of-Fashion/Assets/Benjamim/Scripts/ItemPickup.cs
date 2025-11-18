876e00225fc72fbfa5074908fc7474e5e9b4cabf
Jancley222 <jancley.hermes@alunos.sc.senac.br>
2025-11-18 11:16:53 -0300

    Som nas caixas
    Coloquei um som nas caixas quando elas desaparecem

--- A-Memory-of-Fashion/Assets/Benjamim/Scripts/ItemPickup.cs
+++ A-Memory-of-Fashion/Assets/Benjamim/Scripts/ItemPickup.cs
@@ -5,32 +5,55 @@ public class ItemPickup : MonoBehaviour
     public string itemName;
     private bool playerNear = false;
 
+    private AudioSource pickupSound;
+
     void Start()
     {
+        pickupSound = GetComponent<AudioSource>();
+
         if (PlayerPrefs.GetInt(itemName, 0) == 1)
+        {
             gameObject.SetActive(false);
+        }
     }
 
     void Update()
     {
         if (playerNear && Input.GetKeyDown(KeyCode.E))
         {
-            Inventory.instance.AddItem(itemName);
-            PlayerPrefs.SetInt(itemName, 1);
-            PlayerPrefs.Save();
-            gameObject.SetActive(false);
+            if (pickupSound != null && pickupSound.clip != null)
+            {
+                pickupSound.Play();
+            }
+            float soundDuration = pickupSound.clip.length;
+
+            Invoke("CollectItem", soundDuration);
+
+            enabled = false;
         }
     }
+    void CollectItem()
+    {
+        Inventory.instance.AddItem(itemName);
+        PlayerPrefs.SetInt(itemName, 1);
+        PlayerPrefs.Save();
+        gameObject.SetActive(false);
+    }
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
+        {
             playerNear = true;
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
+        {
             playerNear = false;
+        }
     }
 }
